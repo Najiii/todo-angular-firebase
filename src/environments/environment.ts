65a371f226d072428aa03c4193a0db57f20e5b62
Najiii <54370141+Najiii@users.noreply.github.com>
2020-04-25 21:42:09 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyD2QmcF5G8b6LL2ACWaVASVfV1JeBIur-w",
-    authDomain: "itodo-5b794.firebaseapp.com",
-    databaseURL: "https://itodo-5b794.firebaseio.com",
-    projectId: "itodo-5b794",
-    storageBucket: "itodo-5b794.appspot.com",
-    messagingSenderId: "324548579529",
-    appId: "1:324548579529:web:604b8264a3c8aad958fd18"
-  }
+  production: false
 };
 
 /*
